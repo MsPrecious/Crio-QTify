79555287235e9988fa397c9dad23ed0293174ea5
Anmol <pandeanmol26@gmail.com>
2023-08-30 20:58:08 +0530

    cardCarouselTopAlbums-qtify-takehome

--- qtify/src/components/AlbumGrid.js
+++ qtify/src/components/AlbumGrid.js
@@ -1,39 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Card from './Card';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Card from './Card';
+import '../App.css'; // Import your custom CSS file
 
 const AlbumGrid = () => {
-  const [albums, setAlbums] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState('All');
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [showAll, setShowAll] = useState(false);
 
   useEffect(() => {
-    const fetchAlbums = async () => {
+    // Fetch top albums from the API endpoint
+    const fetchTopAlbums = async () => {
       try {
         const topAlbumsResponse = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
-        const newAlbumsResponse = await axios.get('https://qtify-backend-labs.crio.do/albums/new');
-
-        const topAlbums = topAlbumsResponse.data;
-        const newAlbums = newAlbumsResponse.data;
-
-        setAlbums([...topAlbums, ...newAlbums]);
+        setTopAlbums(topAlbumsResponse.data);
       } catch (error) {
-        console.error('Error fetching albums:', error);
+        console.error('Error fetching top albums:', error);
       }
     };
 
-    fetchAlbums();
+    fetchTopAlbums();
   }, []);
 
-  const filteredAlbums = selectedGenre === 'All'
-    ? albums
-    : albums.filter(album => album.genre && album.genre.key === selectedGenre);
-
-    const topAlbums = filteredAlbums.filter(album => album.isTopAlbum); 
-    const newSongs = filteredAlbums.filter(album => album.isNewSong); 
-    
+  const handleShowAllToggle = async () => {
+    if (!showAll) {
+      try {
+        const allTopAlbumsResponse = await axios.get('https://qtify-backend-labs.crio.do/albums/all-top');
+        setTopAlbums(allTopAlbumsResponse.data);
+      } catch (error) {
+        console.error('Error fetching all top albums:', error);
+      }
+    } else {
+      const topAlbumsResponse = await axios.get('https://qtify-backend-labs.crio.do/albums/top');
+      setTopAlbums(topAlbumsResponse.data);
+    }
+    setShowAll(!showAll);
+  };
 
   const settings = {
     dots: false,
@@ -41,53 +45,49 @@ const AlbumGrid = () => {
     speed: 500,
     slidesToShow: 6,
     slidesToScroll: 6,
-    prevArrow: <div className="slick-prev">{"<"}</div>,
-    nextArrow: <div className="slick-next">{">"}</div>,
+    prevArrow: <div className="navigation-arrow left">{"<"}</div>,
+    nextArrow: <div className="navigation-arrow right">{">"}</div>,
   };
 
   return (
-    <div>
-      {/* Filter section */}
-      <div className="filter-section">
-        <p className="filter-heading">Songs:</p>
-        <div className="filter-buttons">
-          <button onClick={() => setSelectedGenre('All')}>All</button>
-          <button onClick={() => setSelectedGenre('Rock')}>Rock</button>
-          <button onClick={() => setSelectedGenre('Pop')}>Pop</button>
-          <button onClick={() => setSelectedGenre('Jazz')}>Jazz</button>
-          <button onClick={() => setSelectedGenre('Blues')}>Blues</button>
+    <div className="album-grid-container">
+      <div className="album-grid-header">
+        <h2 className="top-albums-heading">Top Albums</h2>
+        <div className="show-all-button" onClick={handleShowAllToggle}>
+          {showAll ? 'Collapse' : 'Show All'}
         </div>
       </div>
-
-      {/* Top Albums Slider */}
-      <div className="card-slider">
-        <h2>Top Albums</h2>
-        <Slider {...settings}>
-          {topAlbums.map(album => (
-            <div key={album.id}>
+      {showAll ? (
+        <div className="album-grid show-all">
+          {topAlbums.map((album, index) => (
+            <Card
+              key={album.id}
+              imageSrc={album.image}
+              followsText={`${album.follows} Follows`}
+              albumTitle={album.title}
+              showAll={showAll}
+              index={index}
+              totalAlbums={topAlbums.length}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="slider-container">
+          <Slider {...settings}>
+            {topAlbums.map((album, index) => (
               <Card
+                key={album.id}
                 imageSrc={album.image}
                 followsText={`${album.follows} Follows`}
+                albumTitle={album.title}
+                showAll={showAll}
+                index={index}
+                totalAlbums={topAlbums.length}
               />
-            </div>
-          ))}
-        </Slider>
-      </div>
-
-      {/* New Songs Slider */}
-      <div className="card-slider">
-        <h2>New Songs</h2>
-        <Slider {...settings}>
-          {newSongs.map(song => (
-            <div key={song.id}>
-              <Card
-                imageSrc={song.image}
-                followsText={`${song.follows} Follows`}
-              />
-            </div>
-          ))}
-        </Slider>
-      </div>
+            ))}
+          </Slider>
+        </div>
+      )}
     </div>
   );
 };
--- qtify/src/components/Card.js
+++ qtify/src/components/Card.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../App.css';
 
-const Card = ({ imageSrc, followsText }) => {
+const Card = ({ imageSrc, followsText, albumTitle }) => {
   return (
     <div className="card">
       <div className="card-image-container">
         <img src={imageSrc} alt="album cover" className="card-image" />
+        <div className="follows-capsule">
+          <p className="follows-text">{followsText}</p>
+        </div>
       </div>
-      <div className="follows-capsule">
-        <p className="follows-text">{followsText}</p>
-      </div>
+      <p className="outside-text">{albumTitle}</p>
     </div>
   );
 }
